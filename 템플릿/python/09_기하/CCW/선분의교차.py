2e7c1d170a9bebc7683da3b82292d00fe8e080f4
jigglypop <donghwanyeom@gmail.com>
2025-04-22 03:28:51 +0900

    러스트 추가

--- 템플릿/python/09_기하/CCW/선분의교차.py
+++ 템플릿/python/09_기하/CCW/선분의교차.py
@@ -1,13 +1,26 @@
 import sys
-sys.stdin = open("17386.txt", "r")
+sys.stdin = open("템플릿/python/09_기하/CCW/17386.txt", "r")
 input = sys.stdin.readline
 
-def ccw(x1, y1, x2, y2, x3, y3):
-    return (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)
+class P:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def ccw(A, B, C):
+    return (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x)
+
+def is_intersect(A, B, C, D):
+    return ccw(A, B, C) * ccw(A, B, D) < 0 and ccw(C, D, A) * ccw(C, D, B) < 0
 
 x1, y1, x2, y2 = map(int, input().split())
-x3, y3, x4, y4 = map(int, input().split())
-if ccw(x1, y1, x2, y2, x3, y3) * ccw(x1, y1, x2, y2, x4, y4) < 0 and ccw(x3, y3, x4, y4, x1, y1)*ccw(x3, y3, x4, y4, x2, y2) < 0:
+x3, y3, x4, y4 = map(int, input().split())    
+A = P(x1, y1)
+B = P(x2, y2)
+C = P(x3, y3)
+D = P(x4, y4)
+
+if is_intersect(A, B, C, D):
     print(1)
 else:
     print(0)
